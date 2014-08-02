55d581a47471e4b0b0411f056c2fc3fad0d99cb4
jjmustc <jjmustc@hotmail.com>
2014-08-01 21:28:19 -0700

    add MyLeet
    add myleet

---
+++ MyLeet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructure
+{
+    public class MyLeet
+    {
+        public List<string> GenerateParentheses(int number)
+        {
+            List<string> results = new List<string>();
+
+            GetParentheses(results, "", number, 0, 0);
+
+            return results;
+        }
+
+        private void GetParentheses(List<string> results, string oneResult, int number, int leftNumber, int rightNumber)
+        {
+
+            if (leftNumber == number && rightNumber == number)
+            {
+                results.Add(oneResult);
+                return;
+            }
+
+            if (leftNumber < number)
+            {
+                oneResult += "(";
+                GetParentheses(results, oneResult, number, leftNumber + 1, rightNumber);
+            }
+
+            if (rightNumber < number && rightNumber < number)
+            {                
+                oneResult += ")";
+                GetParentheses(results, oneResult, number, leftNumber, rightNumber + 1);
+            }
+
+        }
+    }
+}
